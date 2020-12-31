e4fd281bbdad4fdcb1f29d2f4b1fd703448526bf
Kanish Kumar <71805719+KeenKanish@users.noreply.github.com>
2020-12-31 15:53:38 +0530

    Add files via upload

--- keen.py
+++ keen.py
@@ -1,16 +1,27 @@
+# This v1.0 of Mr. Ajeeb Created by Keen Kanish
 import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
 import pywhatkit
-from youtube_search import YoutubeSearch
+import pyaudio
+import pyjokes
 
 engine = pyttsx3.init()
 engine.setProperty("rate", 150)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[2].id)
 
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='This is Mr. Ajeeb Please Close this window to continue')
+
+TestApp().run()
 
 
 def speak(audio):
@@ -28,7 +39,7 @@ def wishMe():
     else:
         speak("Good Evening!")
         
-    speak("I am Mr. Ajeeb sir. I am created by Keen Kanish. How may I Help You?")
+    speak("I am Mr. Ajeeb sir. I was created by Keen Kanish. How may I Help You?")
     
 def takeCommand():
     r = sr.Recognizer()
@@ -68,15 +79,49 @@ if __name__ == "__main__":
             results = pywhatkit.playonyt(query)
         
         elif 'open youtube' in query:
+            speak("Opening youtube...")
             webbrowser.open("https://www.youtube.com")
+
             
         elif 'open google' in query:
+            speak("Opening Google...")
             webbrowser.open("https://www.google.com")
             
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
+            print(f"Sir, the time is {strTime}")
             
+        elif 'thank you' in query:
+            speak("Oh! That's my pleasure!")
+            print("Oh! That's my pleasure!")
+        
+        elif 'thanks' in query:
+            speak("Oh! That's my pleasure!")
+            print("Oh! That's my pleasure!")
+            
+        elif 'relationship' in query:
+            speak("Yes! I am in realtionship with wifi")       
+            print("Yes! I am in realtionship with wifi")
+            
+        elif 'your name' in query:
+            speak("My name is Mr. Ajeeb")        
+            print("My name is Mr. Ajeeb")   
+            
+        elif 'your age' in query:
+            speak("I was created on 30st December 2020. So you can calculate it yourself")         
+            print("I was created on 30st December 2020. So you can calculate it yourself")
+            
+        elif 'old' in query:
+            speak("I was created on 30st December 2020. So you can calculate it yourself")         
+            print("I was created on 30st December 2020. So you can calculate it yourself")    
+            
+        elif 'joke' in query:
+             speak("Grinding it" + pyjokes.get_joke())
+             print("Grinding it" + pyjokes.get_joke())
+                      
         else:
             pass   
+
+
 
